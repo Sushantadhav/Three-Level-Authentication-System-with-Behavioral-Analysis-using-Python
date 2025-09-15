5733d4646cdf7f5b5ef45da2a83ae63f0fa93e18
Sushantadhav <167027275+Sushantadhav@users.noreply.github.com>
2025-09-15 19:53:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,12 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
+    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
+
+    
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///" + os.path.join(BASE_DIR, "database.db")
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
- 
